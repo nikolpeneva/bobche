9feff11bdafc37e08a0719183285e03b3efe9ecd
nikolp18 <npeneva08@gmail.com>
2024-06-02 22:05:02 +0300

    login/register stuff

---
+++ app.py
@@ -0,0 +1,102 @@
+from flask import Flask, render_template, flash, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_ckeditor import CKEditor
+from flask_migrate import Migrate
+from datetime import datetime
+from webforms import LoginForm, UserForm
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash 
+
+
+app = Flask(__name__)
+ckeditor = CKEditor(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = "samo levski"
+
+db = SQLAlchemy(app)
+app.app_context().push
+migrate = Migrate(app, db)
+
+class Posts(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(255))
+	content = db.Column(db.Text)
+	author = db.Column(db.String(255))
+	date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+	slug = db.Column(db.String(255))
+	poster_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class Users(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), nullable=False, unique=True)
+	name = db.Column(db.String(200), nullable=False)
+	email = db.Column(db.String(120), nullable=False, unique=True)
+	password_hash = db.Column(db.String(128))
+	posts = db.relationship('Posts', backref='poster')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+	return Users.query.get(int(user_id))
+
+@app.route('/admin')
+@login_required
+def admin():
+	id = current_user.id
+	if id == 1:
+		return render_template("admin.html")
+	else:
+		flash("Sorry you must be the Admin to access this page")
+		return redirect(url_for('dashboard'))
+	
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = Users.query.filter_by(username=form.username.data).first()
+		if user:
+			if check_password_hash(user.password_hash, form.password.data):
+				login_user(user)
+				flash("Login Succesfull")
+				return redirect(url_for('dashboard'))
+			else:
+				flash("Wrong Password - Try Again")
+		else:
+			flash("User Not Found - Try Again")
+
+
+	return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+	logout_user()
+	flash("You Have Been Logged Out")
+	return redirect(url_for('login'))
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def register():
+	name = None
+	form = UserForm()
+	if form.validate_on_submit():
+		user = Users.query.filter_by(email=form.email.data).first()
+		if user is None:
+			hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
+			user = Users(username=form.username.data, name=form.name.data, email=form.email.data, password_hash=hashed_pw)
+			db.session.add(user)
+			db.session.commit()
+		name = form.name.data
+		form.name.data = ''
+		form.username.data = ''
+		form.email.data = ''
+		form.password_hash.data = ''
+
+		flash("User Added Successfully")
+	our_users = Users.query.order_by(Users.date_added)
+	return render_template("add_user.html", 
+		form=form,
+		name=name,
+		our_users=our_users)
---
+++ webforms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.widgets import TextArea
+from flask_ckeditor import CKEditorField
+from flask_wtf.file import FileField
+
+class LoginForm(FlaskForm):
+	username = StringField("Username", validators=[DataRequired()])
+	password = PasswordField("Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+	
+class UserForm(FlaskForm):
+	name = StringField("Name", validators=[DataRequired()])
+	username = StringField("Username", validators=[DataRequired()])
+	email = StringField("Email", validators=[DataRequired()])
+	favorite_color = StringField("Favorite Color")
+	about_author = TextAreaField("About Author")
+	password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Passwords Must Match!')])
+	password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
+	profile_pic = FileField("Profile Pic")
+	submit = SubmitField("Submit")
